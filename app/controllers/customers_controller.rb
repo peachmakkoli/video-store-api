cbe446310aa7b28bbc4ab6e6aab512659afd21c7
peachmakkoli <lee.higgins.dev@gmail.com>
2020-05-27 10:17:23 -0700

    defines customers#index action, gets a list of customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,8 @@
 class CustomersController < ApplicationController
   def index
-    render json: { ok: "it works!" }, status: :ok
+    customers = Customer.order(:name)
+
+    render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :videos_checked_out_count]), 
+           status: :ok
   end
 end
