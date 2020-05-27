8148bb59d1967a05b2c1245042b63e9344171bfb
peachmakkoli <lee.higgins.dev@gmail.com>
2020-05-27 10:11:53 -0700

    adds tests checking that customers#index returns proper fields

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -10,5 +10,29 @@ describe CustomersController do
       must_respond_with :ok
       expect(response.header['Content-Type']).must_include 'json'
     end
+
+    it "will return all the proper fields for a list of customers" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Array
+
+      body.each do |customer|
+        expect(customer).must_be_instance_of Hash
+        expect(customer.keys.sort).must_equal CUSTOMER_FIELDS
+      end
+    end
+
+    it "returns an empty array if no customers exist" do
+      Customer.destroy_all
+
+      get customers_path
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Array
+      expect(body.length).must_equal 0
+    end
   end
 end
