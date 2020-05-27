59be3092ff25449aa79abdb33804f4e910bbfdfd
peachmakkoli <lee.higgins.dev@gmail.com>
2020-05-27 13:33:12 -0700

    adds test for videos#show action

--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -3,35 +3,65 @@ require "test_helper"
 describe VideosController do
   VIDEO_FIELDS = ["id", "title", "release_date", "available_inventory"].sort
 
-  it "must get index" do
-    get videos_path
+  describe "index" do
+    it "must get index" do
+      get videos_path
 
-    must_respond_with :success
-    expect(response.header['Content-Type']).must_include 'json'
-  end
+      must_respond_with :success
+      expect(response.header['Content-Type']).must_include 'json'
+    end
 
-  it "will return all the proper fields for a list of videos" do
-    get videos_path
+    it "will return all the proper fields for a list of videos" do
+      get videos_path
 
-    body = JSON.parse(response.body)
+      body = JSON.parse(response.body)
 
-    expect(body).must_be_instance_of Array
+      expect(body).must_be_instance_of Array
 
-    body.each do |video|
-      expect(video).must_be_instance_of Hash
-      expect(video.keys.sort).must_equal VIDEO_FIELDS
+      body.each do |video|
+        expect(video).must_be_instance_of Hash
+        expect(video.keys.sort).must_equal VIDEO_FIELDS
+      end
+    end
+
+    it "returns an empty array if no videos exist" do
+      Video.destroy_all
+
+      get videos_path
+
+      body = JSON.parse(response.body)
+
+      must_respond_with :success
+      expect(body).must_be_instance_of Array
+      expect(body.length).must_equal 0
     end
   end
 
-  it "returns an empty array if no videos exist" do
-    Video.destroy_all
+  describe "show" do
+    it "will return a hash with the proper fields for an existing video" do
+      video = videos(:inception)
 
-    get videos_path
+      get video_path(video.id)
 
-    body = JSON.parse(response.body)
+      must_respond_with :ok
 
-    must_respond_with :success
-    expect(body).must_be_instance_of Array
-    expect(body.length).must_equal 0
+      body = JSON.parse(response.body)
+
+      expect(response.header['Content-Type']).must_include 'json'
+      expect(body).must_be_instance_of Hash
+      expect(body.keys.sort).must_equal VIDEO_FIELDS
+    end
+
+    it "will respond with a 404 status for a non-existent video" do
+      get video_path(-1)
+
+      must_respond_with :not_found
+      
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Hash
+      expect(body['ok']).must_equal false
+      expect(body['message']).must_equal 'Not found'
+    end
   end
 end
