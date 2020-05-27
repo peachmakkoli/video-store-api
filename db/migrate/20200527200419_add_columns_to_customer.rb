f81f1021fb2a43aef03e390f25a74299859dbd6d
peachmakkoli <lee.higgins.dev@gmail.com>
2020-05-27 13:06:32 -0700

    generates migration for adding address, city, state to customers

---
+++ db/migrate/20200527200419_add_columns_to_customer.rb
@@ -0,0 +1,7 @@
+class AddColumnsToCustomer < ActiveRecord::Migration[6.0]
+  def change
+    add_column :customers, :address, :string
+    add_column :customers, :city, :string
+    add_column :customers, :state, :string
+  end
+end
