88cd2b466c806a815a40f113e8f7c8f6ccfcae43
dnguye2 <dnguyen1093@gmail.com>
2020-05-27 13:15:21 -0700

    updates the change date datatype migration by implementing 'using cast' syntax

--- db/migrate/20200527200806_change_date_fields_to_date_datatype.rb
+++ db/migrate/20200527200806_change_date_fields_to_date_datatype.rb
@@ -1,6 +1,6 @@
 class ChangeDateFieldsToDateDatatype < ActiveRecord::Migration[6.0]
   def change
-    change_column :videos, :release_date, :date
-    change_column :rentals, :due_date, :date
+    change_column :videos, :release_date, 'date USING CAST(release_date AS date)'
+    change_column :rentals, :due_date, 'date USING CAST(due_date AS date)'
   end
 end
