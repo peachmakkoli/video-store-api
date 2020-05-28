93aa81594b9e0e333ad42117de1fc34291e0ecd9
peachmakkoli <lee.higgins.dev@gmail.com>
2020-05-28 15:48:18 -0700

    adds validations for :total_inventory and :available_inventory, must be present

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,5 @@
 class Video < ApplicationRecord
-  validates :title, :overview, :release_date, presence: true
+  validates :title, :overview, :release_date, :total_inventory, :available_inventory, presence: true
   validates :available_inventory, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   
   has_many :rentals
