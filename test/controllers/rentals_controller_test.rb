1bf8ecce56d525ea0e44999ee411f8e7517f8ce5
dnguye2 <dnguyen1093@gmail.com>
2020-05-28 21:09:33 -0700

    broke up validation test of checkout fields into smaller tests

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,19 +1,9 @@
 require "test_helper"
 
 describe RentalsController do
-  RENTAL_CHECKOUT_FIELDS = ["customer_id", "video_id", "due_date", "videos_checked_out_count", "available_inventory"].sort
+  CHECKOUT_FIELDS = ["customer_id", "video_id", "due_date", "videos_checked_out_count", "available_inventory"].sort
 
   describe "checkout" do
-    before do
-      @expected_check_out_rental = {
-        "customer_id" => customers(:lee).id,
-        "video_id" => videos(:inception).id,
-        "due_date" => (Time.now + 7.days).strftime("%Y-%m-%d"),
-        "videos_checked_out_count" => customers(:lee).videos_checked_out_count + 1,
-        "available_inventory" => videos(:inception).available_inventory - 1
-      }
-    end
-
     let(:rental_data) {
       {
        customer_id: customers(:lee).id,
@@ -21,18 +11,43 @@ describe RentalsController do
       }
     }
 
-    it "can create a rental with the correct information when a checkout is made" do
+    it "can create a rental with the correct fields when a checkout is made" do
       expect { 
         post checkout_path, params: rental_data 
       }.must_differ "Rental.count", 1
       
       body = check_response(expected_type: Hash, expected_status: :success)
 
-      assert_equal(@expected_check_out_rental, body)
+
+      CHECKOUT_FIELDS.each do |field|
+        assert_equal(true, body.has_key?(field))
+      end
     end
 
-    it "returns back detailed errors and a status 404: Not Found if the customer does not exist" do
-      Customer.destroy_all
+    it "creates a rental with a due date that is 7 days from the current date" do
+      post checkout_path, params: rental_data 
+
+      rental = check_response(expected_type: Hash, expected_status: :success)
+
+      assert_equal((Time.now + 7.days).strftime("%Y-%m-%d"), rental["due_date"])
+    end
+
+    it "increments a customer's videos_checked_out_count by 1" do
+      expect { 
+        post checkout_path, params: rental_data 
+        customers(:lee).reload
+      }.must_differ "customers(:lee).videos_checked_out_count", 1
+    end
+
+    it "decrements a videos's available_inventory by 1" do
+      expect { 
+        post checkout_path, params: rental_data 
+        videos(:inception).reload
+      }.must_differ "videos(:inception).available_inventory", -1
+    end
+
+    it "returns errors messages and a status 404: Not Found if the customer does not exist" do
+      rental_data[:customer_id] = -505
 
       expect {
         post checkout_path, params: rental_data
@@ -41,8 +56,8 @@ describe RentalsController do
       body = check_response(expected_type: Hash, expected_status: :not_found)
     end
 
-    it "return back detailed errors and a status 404: Not Found if the video does not exist" do
-      Video.destroy_all
+    it "return errors messages and a status 404: Not Found if the video does not exist" do
+      rental_data[:video_id] = -807
 
       expect {
         post checkout_path, params: rental_data
@@ -51,7 +66,7 @@ describe RentalsController do
       body = check_response(expected_type: Hash, expected_status: :not_found)
     end
 
-    it "returns back detailed errors and a status 400: Bad Request if the video does not have any available inventory before check out" do
+    it "returns errors messages and a status 400: Bad Request if the video does not have any available inventory before check out" do
       rental_data[:video_id] = videos(:rested).id
 
       expect {
