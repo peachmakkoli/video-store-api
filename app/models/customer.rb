d63a15767c8500d8a7862ef9b907173ddaf26dda
peachmakkoli <lee.higgins.dev@gmail.com>
2020-05-28 17:12:35 -0700

    adds dependent:nullify to rental relationship in customer and video models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ApplicationRecord
   validates :videos_checked_out_count, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
-  has_many :rentals
+  has_many :rentals, dependent: :nullify # if a customer is destroyed, the customer_id on associated rentals will be set to nil
   has_many :videos, through: :rentals
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,6 @@ class Video < ApplicationRecord
   validates :title, :overview, :release_date, :total_inventory, :available_inventory, presence: true
   validates :available_inventory, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   
-  has_many :rentals
+  has_many :rentals, dependent: :nullify # if a video is destroyed, the video_id on associated rentals will be set to nil
   has_many :customers, through: :rentals
 end
